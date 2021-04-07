b7bdf52100e02faf08f1417569cc2586b2736147
devikapoduval <77113861+devikapoduval@users.noreply.github.com>
2021-04-07 16:59:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ last_position_of_y=e.touches[0].clientY- canvas.offsetTop;
         ctx.moveTo(last_position_of_x, last_position_of_y);
 
         console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
         ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
         ctx.stroke();
         
